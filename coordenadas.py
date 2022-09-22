90d7facb106630a4e68a25f26eae55813788598e
Vale López <101349987+vale-herrero@users.noreply.github.com>
2022-09-22 17:57:45 +0000

    intento de coordenadas

---
+++ coordenadas.py
@@ -0,0 +1,12 @@
+class punto(x,y):
+  x= int(input("Ingrese el valor de X: "))
+  y= int(input("Ingrese el valor de Y: "))
+
+  if x == 9 or x== 8 or x==7 or x== 6 or x== 5 or x== 4 or x==3 or x==2 or x == 1:
+      x = x
+    else:
+      x = 0
+        if y == 9 or y == 8 or y ==7 or y == 6 or y == 5 or y == 4 or y ==3 or y ==2 or y == 1:
+      y = y
+    else:
+      y = 0
